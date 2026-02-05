7d94989b601c65aa7b0445b4dc6383a2c9fb147e
tkharundore-commits <taherkh.arundore@gmail.com>
2026-02-05 12:23:23 +0100

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Citymapper-horizon-city-app-V4.3/", // <--- AJOUTEZ CETTE LIGNE ICI
   server: {
     host: "::",
     port: 8080,
